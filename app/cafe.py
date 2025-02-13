be3244772625369ac62ee0756c2da06164a23462
Bartolomeu <bartoszokroj@wp.pl>
2025-02-13 19:13:03 +0100

    solutiuon

--- app/cafe.py
+++ app/cafe.py
@@ -1,18 +1,19 @@
 import datetime
-
 from app.errors import (NotVaccinatedError,
                         OutdatedVaccineError,
                         NotWearingMaskError)
+
 
 class Cafe():
     def __init__(self, name: str) -> None:
         self.name = name
 
-    def visit_cafe(self, visitor: dict):
-        if 'vaccine' not in visitor.keys():
+    def visit_cafe(self, visitor: dict) -> str:
+        if "vaccine" not in visitor.keys():
             raise NotVaccinatedError("All friends should be vaccinated")
-        elif visitor['vaccine']['expiration_date'] < datetime.date.today():
+        elif visitor["vaccine"]["expiration_date"] < datetime.date.today():
             raise OutdatedVaccineError("All friends should be vaccinated")
-        elif not visitor['wearing_a_mask']:
+        elif not visitor["wearing_a_mask"]:
             raise NotWearingMaskError("All friends should wear mask")
         else:
+            return f"Welcome to {self.name}"
--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from app.cafe import Cafe
-from app.errors import VaccineError, NotWearingMaskError, OutdatedVaccineError, NotVaccinatedError
+from app.errors import VaccineError, NotWearingMaskError
 
 
 def go_to_cafe(friends: list, cafe: Cafe) -> str:
@@ -16,5 +16,4 @@ def go_to_cafe(friends: list, cafe: Cafe) -> str:
     if masks_to_buy > 0:
         return f"Friends should buy {masks_to_buy} masks"
 
-
-    return f"Friends can go to {cafe}"
+    return f"Friends can go to {cafe.name}"
